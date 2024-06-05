f809998c3b9efbbd8aee16786368bf0fbda2aec8
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-06-05 18:45:52 +0300

    Commit #6: Styling and adding photos

--- checkout/core/urls.py
+++ checkout/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path("", views.index, name='index'),
     path("checkout/<int:product_id>/", views.checkout, name='checkout'),
