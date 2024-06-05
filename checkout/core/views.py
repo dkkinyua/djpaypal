b0273cf429eac4b10aa94f1d8ce20ea10539e945
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-06-05 17:44:30 +0300

    Commit #2: Adding views
    Added views.py for index page, checkout, payment successful and failed

--- checkout/core/views.py
+++ checkout/core/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Product
+from django.
 
 # Create your views here.
+def index(request):
+    context = {
+        "products": Product.objects.all()
+    }
+
+    return render(request, "core/index.html", context)
+
+def checkout(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    context = {
+        "product": product
+    }
+
+    return render(request, "core/checkout.html", context)
+
+def payment_successful(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    return render(request, "core/payment_successful.html", {"product": product})
+
+def payment_failed(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    return render(request, "core/payment_failed.html", {"product": product})
