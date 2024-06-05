f350aa17df140cafd93fdb9f26758426883647bf
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-06-05 18:02:48 +0300

    Commit #4: Making initial database migrations
    Initial model migration to db

--- checkout/core/urls.py
+++ checkout/core/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path("checkout/<int:product_id>/", views.checkout, name='checkout'),
-    path("payment-success/<int: product_id>/", views.payment_successful, name='success'),
-    path("payment-failed/<int: product_id>", views.payment_failed, name='failed')
+    path("payment-success/<int:product_id>/", views.payment_successful, name='success'),
+    path("payment-failed/<int:product_id>", views.payment_failed, name='failed')
 ]
