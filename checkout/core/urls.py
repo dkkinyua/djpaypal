192c84e2e2582085c3fc1e753552579663e73ac8
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-06-05 17:52:52 +0300

    Commit #3: Configuring URLs and URLConf
    Configured URLs in core/urls.py(app) and URLConf in checkout/urls.py (project).

---
+++ checkout/core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("checkout/<int:product_id>/", views.checkout, name='checkout'),
+    path("payment-success/<int: product_id>/", views.payment_successful, name='success'),
+    path("payment-failed/<int: product_id>", views.payment_failed, name='failed')
+]
--- checkout/core/views.py
+++ checkout/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-from django.
+
 
 # Create your views here.
 def index(request):
